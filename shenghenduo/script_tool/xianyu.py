70f9f1abc73736b2d84d648e5fd99e41996c54a3
shangshuai <15903071381@163.com>
2025-01-06 11:51:43 +0800

    code update

--- shenghenduo/script_tool/xianyu.py
+++ shenghenduo/script_tool/xianyu.py
@@ -5,10 +5,17 @@
 
 
 
-import time
+import time, json
 
 import requests
 import hashlib
+
+
+
+def md5_encryption(data):
+    md5 = hashlib.md5()  # 创建一个md5对象
+    md5.update(data.encode('utf-8'))  # 使用utf-8编码数据
+    return md5.hexdigest()  # 返回加密后的十六进制字符串
 
 
 headers = {
@@ -31,8 +38,8 @@ cookies = {
     "cookie2": "1c4e46b3e3564236fb8f85fd603d4f4f",
     "xlly_s": "1",
     "mtop_partitioned_detect": "1",
-    "_m_h5_tk": "3959d763edd58aab1954b5e95b6badd7_1736046153394",
-    "_m_h5_tk_enc": "b76976f11f738f7c12f3b9545fa13daf",
+    "_m_h5_tk": "39e359d72a407d05bf0579f9a61805d7_1736080283492",
+    "_m_h5_tk_enc": "3f6b209f5eeb9a5c1316af8e291b0f05",
     "_samesite_flag_": "true",
     "t": "2ee8129c2e20fdfcb45797b9d5a70e39",
     "_tb_token_": "5543e496e8d75",
@@ -44,26 +51,27 @@ cookies = {
     "tfstk": "gVYiCAxVehS6xkv8Bp7sVs6RpAnLC5_fvKUAHZBqY9WQBRU9g-vDLLdTWEdVnKvddj6O6FFcgNTpXrBtCZA2eZD-e43J1C_5uYHJQIPfAa5z0PyAbMWFksVrvq3B1C_bOWeq2HR_nCE7fC723wSFG_bV_OzZtk5lLP547syEts6FusSN0kzFG1zabPJ2TXffLt7VbvSzuTY2RekhBuoSpsKB-1jGUBm7orydg8Bv_74zrexhjTkRKrz2-1ApYcD3rmByVOIRxpuLSZAFipjJjY4MnIRW0MY0QfpygdTh5amzjLJMCF1BAxqcEnR9VejQgrJlSKjy7MV4uNb1CwfezjE5peX93FjEwzjA8UIP7HnIBiQGZK86IS42UBdJ5MTmEJWXX__FiEGzmtjV4krU4Dcbl6lv8oZfb61hela5fbGf_H4xtXqSfG5C6YhntoZfb61heXc3V-sNO1HR."
 }
 
-url = "https://h5api.m.goofish.com/h5/mtop.taobao.idlemtopsearch.pc.search/1.0/"
+# url = "https://h5api.m.goofish.com/h5/mtop.taobao.idlemtopsearch.pc.search/1.0/"
+# 订单接口
+url = "https://h5api.m.goofish.com/h5/mtop.idle.web.trade.bought.list/1.0/"
 
+# data = {
+#     "data": '{"pageNumber":1,"keyword":"苹果16","fromFilter":false,"rowsPerPage":30,"sortValue":"","sortField":"","customDistance":"","gps":"","propValueStr":{},"customGps":"","searchReqFromPage":"pcSearch","extraFilterValue":"{}","userPositionJson":"{}"}'
+# }
 data = {
-    "data": '{"pageNumber":1,"keyword":"苹果16","fromFilter":false,"rowsPerPage":30,"sortValue":"","sortField":"","customDistance":"","gps":"","propValueStr":{},"customGps":"","searchReqFromPage":"pcSearch","extraFilterValue":"{}","userPositionJson":"{}"}'
+    "data": '{"pageNumber":1,"orderStatus":"ALL"}'
 }
-# print(data.get('data'))
-
+t = int(time.time() * 1000)
 token = cookies.get('_m_h5_tk').split('_')[0]
-sign = f'{token}&{int(time.time() * 1000)}&{34839810}&{data.get('data')}'
+sign = f'{token}&{t}&{34839810}&{data.get('data')}'
 # print(sign)
-def md5_encryption(data):
-    md5 = hashlib.md5()  # 创建一个md5对象
-    md5.update(data.encode('utf-8'))  # 使用utf-8编码数据
-    return md5.hexdigest()  # 返回加密后的十六进制字符串
+
 md5_sign = md5_encryption(sign)
 print(md5_sign)
 params = {
     "jsv": "2.7.2",
     "appKey": "34839810",
-    "t": str(int(time.time() * 1000)),
+    "t": str(t),
     "sign": md5_sign, # f'{token}&{int(time.time())&{appKey}&{data}}'
     "v": "1.0",
     "type": "originaljson",
@@ -79,8 +87,22 @@ params = {
 
 
 response = requests.post(url, headers=headers, cookies=cookies, params=params, data=data)
+response_cookies = response.cookies
 
-print(response.text)
+# print(response.text)
+ret = json.loads(response.text).get('ret')
+if '令牌过期' in ret[0]:
+    for cookie in response_cookies:
+        print(cookie.name, cookie.value)
+        cookies[cookie.name] = cookie.value
+    token = cookies.get('_m_h5_tk').split('_')[0]
+    sign = f'{token}&{t}&{34839810}&{data.get('data')}'
+    md5_sign = md5_encryption(sign)
+    params['sign'] = md5_sign
+    response = requests.post(url, headers=headers, cookies=cookies, params=params, data=data)
+    print(response.text)
+resultList = json.loads(response.text).get('data').get('resultList')
+# print(resultList[0])
 
 
 
